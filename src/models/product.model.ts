a1f13343a1f65ac3fdda8adcf9b813e3c2931f74
lenlo121500 <raulc8808@gmail.com>
2025-06-16 12:38:52 +0800

    feat(models): add product model schema

---
+++ src/models/product.model.ts
@@ -0,0 +1,55 @@
+import mongoose, { Schema } from "mongoose";
+import { IProduct } from "../types";
+
+const productSchema = new Schema<IProduct>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    stock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    images: [{ type: String }],
+    seller: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    ratings: {
+      average: {
+        type: Number,
+        default: 0,
+      },
+      count: {
+        type: Number,
+        default: 0,
+      },
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model<IProduct>("Product", productSchema);
+
+export default Product;
